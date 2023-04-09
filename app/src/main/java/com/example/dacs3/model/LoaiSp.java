878b57bba2fea06a2dcff74e0ace9fafa8fc516c
KaiO277 <nghiatran1527@gmail.com>
2023-04-09 14:42:33 +0700

    KaiO

--- app/src/main/java/com/example/dacs3/model/LoaiSp.java
+++ app/src/main/java/com/example/dacs3/model/LoaiSp.java
@@ -1,7 +1,9 @@
 package com.example.dacs3.model;
 
+import com.example.dacs3.utils.Utils;
+
 public class LoaiSp {
-    public  String URL_BASE="http://192.168.1.3/";
+//    public  String URL_BASE="http://192.168.1.3/";
     private int id;
     private String name;
     private String image;
@@ -23,7 +25,7 @@ public class LoaiSp {
     }
 
     public String getImage() {
-        return URL_BASE+ "do_an_co_so_2_vku/public/images/menuhome/"+image;
+        return Utils.URL_BASE+ "do_an_co_so_2_vku/public/images/menuhome/"+image;
     }
 
     public void setImage(String image) {
