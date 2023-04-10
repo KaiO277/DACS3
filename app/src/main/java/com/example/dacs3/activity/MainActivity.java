7a0bee27964963e849da5242c2f68a6e8594c5da
KaiO277 <nghiatran1527@gmail.com>
2023-04-10 09:24:19 +0700

    KaiO

--- app/src/main/java/com/example/dacs3/activity/MainActivity.java
+++ app/src/main/java/com/example/dacs3/activity/MainActivity.java
@@ -156,9 +156,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void ActionViewFlipper() {
         List<String> manquangcao = new ArrayList<>();
-        manquangcao.add(URL_BASE+"do_an_co_so_2_vku/public/uploads/sliders/slider1.jpg");
-        manquangcao.add(URL_BASE+"do_an_co_so_2_vku/public/uploads/sliders/slider2.jpg");
-        manquangcao.add(URL_BASE+"do_an_co_so_2_vku/public/uploads/sliders/slider3.jpg");
+        manquangcao.add(Utils.URL_BASE+"do_an_co_so_2_vku/public/uploads/sliders/slider1.jpg");
+        manquangcao.add(Utils.URL_BASE+"do_an_co_so_2_vku/public/uploads/sliders/slider2.jpg");
+        manquangcao.add(Utils.URL_BASE+"do_an_co_so_2_vku/public/uploads/sliders/slider3.jpg");
         for (int i =0; i<manquangcao.size(); i++){
             ImageView imageView = new ImageView(getApplicationContext());
             Glide.with(getApplicationContext()).load(manquangcao.get(i)).into(imageView);
