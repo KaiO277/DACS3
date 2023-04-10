7ed4d025d0775c692f988ed2503b91e73c0aa0cd
KaiO277 <nghiatran1527@gmail.com>
2023-04-10 07:53:17 +0700

    KaiO

--- app/src/main/java/com/example/dacs3/activity/LienHeActivity.java
+++ app/src/main/java/com/example/dacs3/activity/LienHeActivity.java
@@ -2,15 +2,37 @@ package com.example.dacs3.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.example.dacs3.R;
 
 public class LienHeActivity extends AppCompatActivity {
+    LinearLayout linearLayout,layoutSdt;
+    String url = "http://192.168.1.3/do_an_co_so_2_vku/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lien_he);
+        linearLayout = findViewById(R.id.layout_web);
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+        layoutSdt = findViewById(R.id.layout_sdt);
+        layoutSdt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                startActivity(intent);
+            }
+        });
     }
 }
